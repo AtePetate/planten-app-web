9df89cd2b55a4069422fac63144753e093a0bfe2
AtePetate <alex4tune@hotmail.com>
2025-07-01 15:53:52 +0200

    Rebuild web with images

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e237af97204726dc2c26a6d1e9026e3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "bade4b48f83a42ae5adb58471d95f8fc",
+"flutter_bootstrap.js": "b5e3acff53956f36cedc861ef424e35d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
